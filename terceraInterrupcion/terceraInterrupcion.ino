fb4fb9146f3407dfda7ae6fc8be56fffb831ba9a
cristianhoyos66 <cristianhoyos66@hotmail.com>
2015-11-17 20:04:58 -0500

    feat: begin counter with number given

--- terceraInterrupcion/terceraInterrupcion.ino
+++ terceraInterrupcion/terceraInterrupcion.ino
@@ -1,4 +1,6 @@
 int counter = -1;
+char inChar=0;
+boolean stringComplete = false;
 
 //para display de catodo comun que se prende con unos
 byte Digit[10][7] =                
@@ -7,7 +9,7 @@ byte Digit[10][7] =
    { 0,1,1,0,0,0,0 },    // 1
    { 1,1,0,1,1,0,1 },    // 2
    { 1,1,1,1,0,0,1 },    // 3
-   { 0,0,1,0,0,1,1 },    // 4
+   { 0,1,1,0,0,1,1 },    // 4
    { 1,0,1,1,0,1,1 },    // 5
    { 1,0,1,1,1,1,1 },    // 6
    { 1,1,1,0,0,0,0 },    // 7
@@ -31,12 +33,28 @@ void sum () {
 }
 
 void setup () {
+   Serial.begin(9600);
    for (int i = 4; i<=10; i++){
     pinMode(i, OUTPUT);
    }
    attachInterrupt(0, sum, FALLING);
+   sum();
 }
 
 void loop () {
-    
+  serialEvent();
+  if (stringComplete) {
+    if(inChar>='0' && inChar<='9'){
+      counter = inChar-48;
+      stringComplete = false;
+    }
+  } 
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    inChar = (char)Serial.read();
+    Serial.println(inChar);
+    stringComplete = true;
+  }
 }
